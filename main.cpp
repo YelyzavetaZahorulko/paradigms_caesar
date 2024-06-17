03ca3d320f7fe0e2391a4ebcf4c9d77ea57aadb8
yelyzaveta <yzahorulko@kse.org.ua>
2024-06-17 23:21:40 +0300

    decrypt method

--- main.cpp
+++ main.cpp
@@ -24,6 +24,25 @@ char* encrypt(const char* rawText, int key) {
     return encryptedText;
 }
 
+char* decrypt(char* encryptedText, int key) {
+    int length = std::strlen(encryptedText);
+    char* decryptedText = new char[length + 1];
+
+    for (int i = 0; i < length; ++i) {
+        char ch = encryptedText[i];
+
+        if (ch >= 'a' && ch <= 'z') {
+            decryptedText[i] = 'a' + (ch - 'a' - key + 26) % 26;
+        } else if (ch >= 'A' && ch <= 'Z') {
+            decryptedText[i] = 'A' + (ch - 'A' - key + 26) % 26;
+        } else {
+            decryptedText[i] = ch;
+        }
+    }
+
+    decryptedText[length] = '\0';
+    return decryptedText;
+}
 
 int main() {
     std::string inputText;
@@ -37,11 +56,14 @@ int main() {
     const char* rawText = inputText.c_str();
 
     char* encrypted = encrypt(rawText, key);
+    char* decrypted = decrypt(encrypted, key);
 
     std::cout << "Original: " << rawText << std::endl;
     std::cout << "Encrypted: " << encrypted << std::endl;
+    std::cout << "Decrypted: " << decrypted << std::endl;
 
     delete[] encrypted;
+    delete[] decrypted;
 
     return 0;
 }
